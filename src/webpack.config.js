788164e0cfb6b678c58541b5195945afd5af45e7
Leopold Arreström <leopold@talentcompany.io>
2022-11-22 16:21:31 +0100

    Add sound p5

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -18,6 +18,28 @@ module.exports = {
           },
         },
       },
+      {
+        test: path.resolve(
+            __dirname,
+            "node_modules/p5/lib/addons/p5.sound.js"
+        ),
+        loader: "imports-loader",
+        options: {
+            type: "module",
+            imports: "p5",
+        },
+    },
+    {
+        test: path.resolve(
+            __dirname,
+            "node_modules/p5/lib/addons/p5.sound.min.js"
+        ),
+        loader: "imports-loader",
+        options: {
+            type: "module",
+            imports: "p5",
+        },
+    },
     ],
   },
 };
